588a21ce7e4e89abb98298e3297b98355f0190cc
Esther Alter <alters@mit.edu>
2023-05-10 14:44:04 -0400

    plus?

--- src/text.rs
+++ src/text.rs
@@ -327,7 +327,7 @@ pub fn draw_text_ex(text: &str, x: f32, y: f32, params: TextParams) {
             + (glyph.h as f32 * font_scale_y + font_data.offset_y as f32 * font_scale_y)
                 * angle_rad.sin();
         let top_coord = (font_data.offset_x as f32 * font_scale_x + total_width) * angle_rad.sin()
-            + (0.0 - glyph.h as f32 * font_scale_y)
+            + (0.0 + glyph.h as f32 * font_scale_y - font_data.offset_y as f32 * font_scale_y)
                 * angle_rad.cos();
 
         total_width += font_data.advance * font_scale_x;
