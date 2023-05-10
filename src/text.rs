f385e2911be8a12e1c29d9c705cda380fb8d52b6
Esther Alter <alters@mit.edu>
2023-05-10 14:45:41 -0400

    does anything work??

--- src/text.rs
+++ src/text.rs
@@ -334,7 +334,7 @@ pub fn draw_text_ex(text: &str, x: f32, y: f32, params: TextParams) {
 
         let dest = Rect::new(
             left_coord / dpi_scaling as f32 + x,
-            top_coord / dpi_scaling as f32 + y,
+            top_coord / dpi_scaling as f32 + y * 2.0,
             glyph.w as f32 / dpi_scaling as f32 * font_scale_x,
             glyph.h as f32 / dpi_scaling as f32 * font_scale_y,
         );
